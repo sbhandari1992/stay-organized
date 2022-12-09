5573910f49d5f9d725f840beaf5b34b502f4947f
Sam Bhandari <sambhandari2021@yahooe.com>
2022-12-09 14:27:04 -0500

    merge main to toDoBranch to work with latest code

--- new-to-do.js
+++ new-to-do.js
@@ -1,23 +1,4 @@
-<<<<<<< HEAD
-// let select = document.getElementById('selectUsernames')
-// function selectUserName () {
-//     fetch('http://localhost:8083/api/users')
-//     .then((response) => response.json())
-//     .then(usernames => {
-//         console.log(usernames);
-//         for( let i = 0; usernames.length ; i++){
-//             let opt = usernames[i].username;
-//             let userEl = document.createElement('option');
-//             userEl.textContent = opt;
-//             userEl.value = opt;
-//             select.appendChild(userEl);
-//         }
-//     })
 
-// }
-
-// selectUserName();
-=======
 let select = document.getElementById('selectUsernames')
 function displayUsernames() {
     fetch('http://localhost:8083/api/users')
@@ -50,4 +31,3 @@ function displayCategories() {
 displayCategories();
 
 
->>>>>>> main
