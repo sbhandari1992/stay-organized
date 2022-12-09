991576b18b3304f0688a23cb3901111802d4a908
Sam Bhandari <sambhandari2021@yahooe.com>
2022-12-09 12:04:29 -0500

    make todo list showup as asked and also chance class to id on todo-html page

--- todo.js
+++ todo.js
@@ -1,14 +1,65 @@
+let  selectUsernames = document.getElementById('selectUsernames');
+let displayData = document.getElementById('displayData');
+
 let  allUserUrl = 'http://localhost:8083/api/users';
-let allToDoUrl = 'http://localhost:8083/api/todos';
+let allToDosUrl = 'http://localhost:8083/api/todos';
+let allCategories = 'http://localhost:8083/api/categories'
 
 
 fetch(allUserUrl)
 .then(response => response.json())
 .then(data => {
-    console.log(data);
-    let nameOpt = new Option();
-    console.log(nameOpt);
+    data.forEach(ar => {
+        let nameOpt = new Option(ar.name);
+        selectUsernames.appendChild(nameOpt);
+    });
+
+    selectUsernames.onchange = getUserAndDisplayToDoList;
+
+    function getUserAndDisplayToDoList() {
+        data.forEach(ar => {
+            if (selectUsernames.value == ar.name) {
+                let iD = ar.id;
+                fetch(allToDosUrl + '/' + iD)
+                       .then(response => response.json())
+                       .then(data => {
+                        displayData.innerHTML = " ";
+ 
+                        let userID = document.createElement('p');
+                        userID.innerHTML = `userID: ${data.userid}`;
+                        displayData.appendChild(userID);
+
+                        let Category = document.createElement('p');
+                        Category.innerHTML = `Category: ${data.category}`;
+                        displayData.appendChild(Category);
+
+                        let Description = document.createElement('p');
+                        Description.innerHTML = `Description: ${data.description}`;
+                        displayData.appendChild(Description);
+
+                        let Deadline = document.createElement('p');
+                        Deadline.innerHTML = `Deadline: ${data.deadline}`;
+                        displayData.appendChild(Deadline);
+
+                        let Priority = document.createElement('p');
+                        Priority.innerHTML = `Priority: ${data.priority}`;
+                        displayData.appendChild(Priority);
+
+                        let Completed = document.createElement('p');
+                        // data.completed = true ? Completed.innerHTML = "YES" : Completed.innerHTML = "NO";
+                        Completed.innerHTML = `Completed: ${data.completed}`;
+                        displayData.appendChild(Completed);
+                        
+
+                       })
+
+            }
+
+        })
+         
+    }
 })
+
 
 
 
