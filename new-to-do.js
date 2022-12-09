d2e35031643b1746523f69a25bf542ad3e357313
Sam Bhandari <sambhandari2021@yahooe.com>
2022-12-09 14:06:43 -0500

    comment new to-do .js

--- new-to-do.js
+++ new-to-do.js
@@ -1,18 +1,18 @@
-let select = document.getElementById('selectUsernames')
-function selectUserName () {
-    fetch('http://localhost:8083/api/users')
-    .then((response) => response.json())
-    .then(usernames => {
-        console.log(usernames);
-        for( let i = 0; usernames.length; i++){
-            let opt = usernames[i].username;
-            let userEl = document.createElement('option');
-            userEl.textContent = opt;
-            userEl.value = opt;
-            select.appendChild(userEl);
-        }
-    })
+// let select = document.getElementById('selectUsernames')
+// function selectUserName () {
+//     fetch('http://localhost:8083/api/users')
+//     .then((response) => response.json())
+//     .then(usernames => {
+//         console.log(usernames);
+//         for( let i = 0; usernames.length ; i++){
+//             let opt = usernames[i].username;
+//             let userEl = document.createElement('option');
+//             userEl.textContent = opt;
+//             userEl.value = opt;
+//             select.appendChild(userEl);
+//         }
+//     })
 
-}
+// }
 
-selectUserName();
+// selectUserName();
