dfabc8a71e20a52bee5b73713f027363a63bd538
niyaridout55@gmail.com <Niya Ridout>
2022-12-10 12:14:49 -0500

    change the api address and change dropdown value to userid

--- to-do.js
+++ to-do.js
@@ -1,13 +1,13 @@
 let select = document.getElementById('selectNames')
 function displayNames() {
-    fetch('http://localhost:8083/api/users')
+    fetch('http://localhost:8083/api/todos')
         .then((response) => response.json())
         .then(data => {
             console.log(data);
             for (let i = 0; i < data.length; i++) {
                 let nameOpts = document.createElement('option');
                 nameOpts.textContent = data[i].name;
-                nameOpts.value = data[i].id;
+                nameOpts.value = data[i].userid;
                 select.appendChild(nameOpts);
             }
         })
